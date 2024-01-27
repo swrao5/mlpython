011a718d9ab1d70668912dd6e87a38bbc45a877d
SWAROOP RAO <37587563+swrao5@users.noreply.github.com>
2024-01-27 13:07:40 +0530

    added additional scripts

---
+++ mnist_clf/bin/run.sh
@@ -0,0 +1,5 @@
+python mnist_clf\src\train.py --fold 0
+python mnist_clf\src\train.py --fold 1
+python mnist_clf\src\train.py --fold 2
+python mnist_clf\src\train.py --fold 3
+python mnist_clf\src\train.py --fold 4
---
+++ mnist_clf/src/config.py
@@ -0,0 +1,2 @@
+TRAINING_FILE = "mnist_clf\input\mnist_train_folds.csv"
+MODEL_OUTPUT = "mnist_clf\models"
